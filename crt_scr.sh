5788fe8d47df58783d37f9292dba31bd4afd689c
marty <shukovsergey1995@gmail.com>
2020-04-06 17:44:48 +0300

    change

--- crt_scr.sh
+++ crt_scr.sh
@@ -1,16 +1,21 @@
-#! /bin/bash +x
+#! /bin/bash
 
 PATH_DIR="$HOME/bin";
+
+if [[ $# == 0 ]]; then
+	echo "$(basename $0 .sh) Usage -[i|n name]";
+	exit 1;
+fi	
 
 function check_name {
 	echo -n "input name of command> "; 
 	read name_com;
 	   	if [[ !("$name_com" =~ ^[A-Za-z0-9]+\.sh$) ]]; then 
-			echo "incorrect name's file";
+			echo "invalid file name";
 			check_name;
 		fi;
 
-		if [[ -f $name_com ]]; then
+		if [[ -f $PATH_DIR/$name_com ]]; then
 			echo "file is exist";
 			check_name;
 		fi;
@@ -20,16 +25,16 @@ function check_mod {
 	echo -n "input mod for file[for example: 777]> "; 
 	read mod;
 		if [[ !("$mod" =~ ^[0-7]{3,4}$) ]]; then
-			echo "incorrect mode";
+			echo "incorrect mod";
 			check_mod;
 		fi;
 }
 
-function add_file {
+function add_script {
 	mod=${2:-"+x"};
 	cd $PATH_DIR;	
 	touch $1;
-	chmod +x $1; 
+	chmod $mod $1; 
 	echo "#! /bin/bash" > $1;
 	vim $1;
 }
@@ -37,17 +42,17 @@ function add_file {
 while getopts "in:" opt; do
 	case $opt in
 		n) if [[ "$OPTARG" =~ ^[A-Za-z0-9]+\.sh$ ]]; then
-			if [[ -f $OPTARG ]]; then
+			if [[ -f $PATH_DIR/$OPTARG ]]; then
 				echo "file is exist";
 				exit 1;
 			fi;
-		   	add_file $OPTARG;
-		   else echo "incorrect name's file"; 	
+		   	add_script $OPTARG;
+		   else echo "invalid file name"; 	
 		   fi ;;
 
 	   	i) check_name; 
 		   check_mod;
-   		   add_file $name_com $mod;	
+   		   add_script $name_com $mod;	
 
         esac
 done	
